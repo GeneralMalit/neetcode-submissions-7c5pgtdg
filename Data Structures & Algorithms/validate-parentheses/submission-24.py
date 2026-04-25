151a591e3362c9c40151fdce6153593c9ea1e88e
GeneralMalit <somni.hax@gmail.com>
2026-04-25 15:56:44 +0800

    Add: validate-parentheses - submission-24

---
+++ Data Structures & Algorithms/validate-parentheses/submission-24.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pairs = {
+            ']':'[',
+            '}':'{',
+            ')':'('
+        }
+        stack = []
+        for i in s:
+            if i in pairs:
+                if not stack or stack[-1] != pairs[i]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(i)
+        return len(stack) == 0
