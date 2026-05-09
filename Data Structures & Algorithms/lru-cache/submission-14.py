aaa09e66072390d3c3d8a7812a338ac31fff3706
GeneralMalit <somni.hax@gmail.com>
2026-05-09 08:06:38 +0800

    Add: lru-cache - submission-14

---
+++ Data Structures & Algorithms/lru-cache/submission-14.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.none = None
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.left, self.right = Node(0, 0),Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.add(self.cache[key])
+            return self.cache[key].val
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        node = Node(key, value)
+        self.cache[key] = node
+        self.add(node)
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+    def remove(self, node):
+        left, right = node.prev, node.next
+        left.next, right.prev = right, left
+    
+    def add(self, node):
+        left, right = self.right.prev, self.right
+        node.prev, node.next = left, right
+        left.next, right.prev = node, node
