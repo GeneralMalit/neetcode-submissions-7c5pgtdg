a4002a96d3b1baea92317e80a6276b87a41621e8
GeneralMalit <somni.hax@gmail.com>
2026-05-18 06:20:40 +0800

    Add: design-word-search-data-structure - submission-36

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-36.py
@@ -0,0 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = {}
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(node, idx):
+            if idx == len(word):
+                return node.word
+
+            char = word[idx]
+            if char == ".":
+                for child in node.children:
+                    if dfs(node.children[child], idx + 1):
+                        return True
+                return False
+
+            else:        
+                if char not in node.children:
+                    return False
+                else:
+                    return dfs(node.children[char], idx + 1)
+            return False
+        
+        res = dfs(self.root, 0)
+        print(f"res = {res} for word = {word}")
+        return res
