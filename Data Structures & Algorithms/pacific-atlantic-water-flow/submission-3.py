7d33603346978c68d94c82851cbfdbbaff02a78c
GeneralMalit <somni.hax@gmail.com>
2026-06-04 09:32:42 +0800

    Add: pacific-atlantic-water-flow - submission-3

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(heights), len(heights[0])
+        dirs = [(1, 0),(-1, 0),(0, 1),(0, -1)]
+        def dfs(starts):
+            a = set(starts)
+            q = deque(a)
+
+            while q:
+                for i in range(len(q)):
+                    r, c = q.popleft()
+                    for dr, dc in dirs:
+                        nr, nc = r + dr, c + dc
+                        if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in a and heights[nr][nc] >= heights[r][c]:
+                            a.add((nr, nc))
+                            q.append((nr, nc)) 
+
+            return a
+
+
+        pacific = [(0, c) for c in range(cols)] + [(r, 0) for r in range(rows)]
+        atlantic = [(rows - 1, c) for c in range(cols)] + [(r, cols - 1) for r in range(rows)]
+
+        pac = dfs(pacific)
+        atl = dfs(atlantic)
+        res = []
+        
+        return list(pac.intersection(atl))
